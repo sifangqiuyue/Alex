511f9cef63557edf9115d545e7cba16e27680b64
sifangqiuyue <1131845423@qq.com>
2025-07-08 19:18:55 +0800

    update if-else in hello-2.py

--- hello-2.py
+++ hello-2.py
@@ -1,4 +1,12 @@
-print("hello world")
-print("你好 paython")
-print("hello")
-print("nihao")
+# print("hello world")
+# print("你好 paython")
+# print("hello")
+# print("nihao")
+a = 5
+b = 5
+if b / a == 2:
+    print("YES, THIS IS 2")
+elif b / a == 1:
+    print("YES, THIS IS 1")
+else:
+    print("NO, THIS IS FALSE")
